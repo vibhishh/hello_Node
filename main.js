5edca580054559a971030e6224ac6547ec6ea855
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-04-18 11:02:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Everyone, This is My Basic Nodejs Web Appliaction test')
 })
 
 app.listen(port, () => {
